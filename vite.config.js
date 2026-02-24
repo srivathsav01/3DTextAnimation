50603a2655d93ed47eaa790cdd31a7d0a2bbe415
Srivathsav M <35376695+srivathsav01@users.noreply.github.com>
2026-02-24 16:14:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import restart from 'vite-plugin-restart'
 
 export default {
+    base: '/3DTextAnimation/',
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
